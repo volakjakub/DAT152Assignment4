5f40f4b22d2e5f8e74b936261b618e02c2f0b19d
Jakub Volák <jakub.volak@seznam.cz>
2022-11-09 15:08:06 +0100

    Added tests.

--- src/test/java/no/hvl/dat152/Assignment4/Assignment4ApplicationTests.java
+++ src/test/java/no/hvl/dat152/Assignment4/Assignment4ApplicationTests.java
@@ -1,13 +1,36 @@
 package no.hvl.dat152.Assignment4;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
 
-@SpringBootTest
+import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest()
+@AutoConfigureMockMvc
 class Assignment4ApplicationTests {
+	@Autowired
+	private MockMvc mvc;
 
 	@Test
-	void contextLoads() {
+	void databaseInitTest() throws Exception {
+		mvc.perform(get("/items")
+						.contentType(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andExpect(content()
+						.contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+				.andExpect(jsonPath("$", hasSize(3)));
 	}
 
+	@Test
+	void ItemNotFoundTest() throws Exception {
+		mvc.perform(get("/items/10100")
+						.contentType(MediaType.APPLICATION_JSON))
+				.andExpect(status().isNotFound());
+	}
 }
